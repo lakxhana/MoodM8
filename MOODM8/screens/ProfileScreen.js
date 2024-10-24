ede8df387723a668269908cf54ddfea3952f2dfb
lakxhanaselvarajah <lakxhanaselvarajah@gmail.com>
2024-10-24 11:51:04 +0800

    main template

---
+++ MOODM8/screens/ProfileScreen.js
@@ -0,0 +1,24 @@
+// screens/ProfileScreen.js
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: '#ab9e7f' }}>Your Profile</Text>
+      {}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+});
+
+export default ProfileScreen;
